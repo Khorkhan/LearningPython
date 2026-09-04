1bb0dbe367084e3f6cc6ec2fb664151e654aa333
Khan <nnookk1235@gmail.com>
2026-09-04 19:52:41 +0700

    Day 3
    เรียนรู้เรื่อง List

---
+++ LearnPython.org/Basic/day3.py
@@ -0,0 +1,28 @@
+# List
+
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
+numbers = []
+strings = []
+name = ["John", "Eric", "Jessica"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("Hello")
+strings.append("World!")
+
+secound_name = name[1]
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % secound_name)
